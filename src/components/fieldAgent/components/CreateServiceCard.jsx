fa898d7977e1f0bb909c133fa873631ca06f2e73
shanjid5566 <shanjidahmed66@gmail.com>
2025-10-20 11:02:32 +0600

    Filter Height adjest in field Agent

--- src/components/fieldAgent/components/CreateServiceCard.jsx
+++ src/components/fieldAgent/components/CreateServiceCard.jsx
@@ -4,7 +4,7 @@ const CreateServiceCard = ({ onCreateService }) => {
     <div className="bg-white p-8 sm:p-12 rounded-2xl w-full mx-auto border border-gray-100">
       <button
         onClick={onCreateService}
-        className="text-[#28A844]  px-5 py-2 rounded mx-auto flex flex-col items-center gap-2 font-medium text-sm md:text-base"
+        className="text-[#28A844]  px-5 py-2 rounded mx-auto flex flex-col items-center gap-2 text-sm md:text-base"
       >
         <MdUploadFile  className=" size-20 md:size-24 mx-auto"/> <p>Create Service</p>
       </button>
